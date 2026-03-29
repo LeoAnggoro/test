01821e4cf0eebd12c7946f0a659df892835152e8
Leo Anggoro <leoanggoro07@gmail.com>
2026-03-29 19:56:35 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import react from "react";
+import "assets/scss/style.css";
+
+function App() {
+  return (
+    <div className="App">
+Hai Dunia</div>
+  );
+}
+
+export default App;
---
+++ src/pages/LandingPages.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+import second from 'elements/Button'
+export default class LandingPages extends Component {
+  render() {
+    return (
+      <div>LandingPages</div>
+    )
+  }
+}
